ff90bec81fc68c9985d2f6ac962d5defaae48b30
Dirk van den Bekerom <dcmvdbekerom@gmail.com>
2026-08-16 14:44:07 +0200

    Update build_win_x64.bat

--- app/build_win_x64.bat
+++ app/build_win_x64.bat
@@ -1,12 +1,12 @@
-set "VK_DIR=C:\VulkanSDK\1.4.357.0"
+
 set "VK_ARM64X=-x64"
 
 cl /LD /MD /EHsc /DVK_API_VERSION=11^
  /I..\..\..\VkFFT-radis\vkFFT^
- /I"%VK_DIR%\Include"^
- /I"%VK_DIR%\Include\glslang\Include"^
+ /I"%VK_SDK_PATH%\Include"^
+ /I"%VK_SDK_PATH%\Include\glslang\Include"^
  vkfft_vulkan.cpp^
- /link /LIBPATH:"%VK_DIR%\Lib%VK_ARM64X%"^
+ /link /LIBPATH:"%VK_SDK_PATH%\Lib%VK_ARM64X%"^
  vulkan-1.lib^
  GenericCodeGen.lib^
  glslang.lib^
